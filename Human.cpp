b54a2f050046c23b821f8f35588a904ce0f84ef2
Abrackadabrr <iliynea2002@ya.ru>
2021-04-14 16:37:02 +0300

    Divided population and human classes into different files
    New:
        Human.cpp
        Human.h
    Changes:
          Population.cpp
          Population.h

---
+++ Human.cpp
@@ -0,0 +1,108 @@
+//
+// Created by evgen on 14.04.2021.
+//
+
+#include "Human.h"
+
+//-----------------------Human-------------------------//
+
+Human::Human(): Human(10, 100){}
+
+Human::Human(unsigned int mentalStability, unsigned int PhysicalHealth)
+{
+    this->MENTAL_STABILITY = mentalStability;
+    this->age = 0;
+
+    this->amount_of_black_accidents = 0;
+    this->type_as_a_worker = -1;
+    this->is_alive = true;
+    this->is_able_to_work = false;
+    this->is_able_to_study = false;
+
+    this->physical_health = PhysicalHealth;
+    this->moral_health = 100;
+    this->year_of_education = -1;
+
+}
+
+Human::Human(unsigned int common_status_of_the_ark):
+        Human(10, 100){
+    this->physical_health *= (common_status_of_the_ark / 100);
+}
+unsigned int Human::getMentalStability() const {
+    return MENTAL_STABILITY;
+}
+
+unsigned int Human::getAge() const {
+    return age;
+}
+
+unsigned int Human::getPhysicalHealth() const {
+    return physical_health;
+}
+
+unsigned int Human::getMoralHealth() const {
+    return moral_health;
+}
+
+unsigned int Human::getTypeAsAWorker() const {
+    return type_as_a_worker;
+}
+
+unsigned int Human::getAmountOfBlackAccidents() const {
+    return amount_of_black_accidents;
+}
+
+unsigned int Human::getYearOfEducation() const {
+    return year_of_education;
+}
+
+bool Human::isAlive() const {
+    return is_alive;
+}
+
+bool Human::isAbleToWork() const {
+    return is_able_to_work;
+}
+
+void Human::setAmountOfBlackAccidents(unsigned int amountOfBlackAccidents) {
+    amount_of_black_accidents = amountOfBlackAccidents;
+}
+
+
+void Human::setYearOfEducation(unsigned int yearOfEducation) {
+    year_of_education = yearOfEducation;
+}
+
+void Human::setAge(int Age) {
+    this->age = Age;
+}
+
+void Human::setPhysicalHealth(unsigned int physicalHealth) {
+    this->physical_health = physicalHealth;
+}
+
+void Human::setMoralHealth(unsigned int moralHealth) {
+    this->moral_health = moralHealth;
+}
+
+void Human::setTypeAsAWorker(unsigned int typeAsAWorker) {
+    this->type_as_a_worker = typeAsAWorker;
+}
+
+void Human::setIsAlive(bool isAlive) {
+    this->is_alive = isAlive;
+}
+
+void Human::setIsAbleToWork(bool isAbleToWork) {
+    is_able_to_work = isAbleToWork;
+}
+
+bool Human::isAbleToStudy() const {
+    return this->is_able_to_study;
+}
+
+void Human::setIsAbleToStudy(bool isAbleToStudy) {
+    this->is_able_to_study = isAbleToStudy;
+}
+
--- Population.h
+++ Population.h
@@ -5,58 +5,9 @@
 #ifndef THE_ARK_POPULATION_H
 #define THE_ARK_POPULATION_H
 #include <vector>
+#include "Human.h"
 
 using std::vector;
-
-class Human {
-private:
-    unsigned int MENTAL_STABILITY; // <- это поле контролирует СС
-
-    unsigned int age;
-    unsigned int physical_health;
-    unsigned int moral_health; // <- это поле контролирует СС
-    unsigned int amount_of_black_accidents; // количество негативных ситуаций, влияющих на человека
-                                                // если их больше, чем моральная стабильность, то моральное здоровье человека уменьшается
-                                                // <- это поле контролирует СС
-
-    unsigned int type_as_a_worker; // давайте условимся: 2 - ученый, 1 - рабочий; 0 - нет профессии
-
-    unsigned int year_of_education; // 0 - не учится
-                                    // <- это поле контролирует СС
-
-    bool is_alive;
-    bool is_able_to_work; // <- это поле контролирует СС
-    bool is_able_to_study; //
-
-public:
-    Human();
-    Human(unsigned int mentalStability, unsigned int PhysicalHealth);
-    Human(unsigned int common_status_of_the_ark);
-
-    unsigned int getMentalStability() const;
-    unsigned int getAge() const;
-    unsigned int getPhysicalHealth() const;
-    unsigned int getMoralHealth() const;
-    unsigned int getTypeAsAWorker() const;
-    unsigned int getAmountOfBlackAccidents() const;
-    unsigned int getYearOfEducation() const;
-    bool isAlive() const;
-    bool isAbleToWork() const;
-    bool isAbleToStudy() const;
-
-    void setAge(int Age);
-    void setPhysicalHealth(unsigned int physicalHealth);
-    void setMoralHealth(unsigned int moralHealth);
-    void setTypeAsAWorker(unsigned int typeAsAWorker);
-    void setIsAlive(bool isAlive);
-    void setIsAbleToWork(bool isAbleToWork);
-    void setIsAbleToStudy(bool isAbleToStudy);
-    void setAmountOfBlackAccidents(unsigned int amountOfBlackAccidents);
-    void setYearOfEducation(unsigned int yearOfEducation);
-
-    friend class Population;
-};
-
 
 class Population {
 private:
