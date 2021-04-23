bd5237299ae007a7c6d6aa0cb46059bfa7fc741c
oitsumerarete <71769533+oitsumerarete@users.noreply.github.com>
2021-04-23 19:44:19 +0300

    Small staff change

--- TechnicalService.cpp
+++ TechnicalService.cpp
@@ -3,6 +3,7 @@
 //
 
 #include "TechnicalService.h"
+#include "TheArk.h"
 
 TechnicalService::TechnicalService()
 {
@@ -21,7 +22,14 @@ void TechnicalService::process_accident(AccidentSeverity as)
     if (protectionState < 15)
         emergencyRepair();
     // kill some people
+    kill(as * staff / 20);
     staff -= as * staff / 20;
+}
+
+void TechnicalService::kill(int victims)
+{
+    staff -= victims;
+    for (int i = 0; i < victims; i++) TheArk::get_instance()->getPopulation()->getAllClassification()[Technical_Service].pop_back();
 }
 
 // считает и возвращает состояние службы в зависимости от количества работающих людей и
@@ -36,6 +44,7 @@ void TechnicalService::emergencyRepair()
     resources -= int(repairing / 100) * maxResources;
     // убить много людей, так как экстренная и масштабная починка
     staff = int(0.9 * staff);
+    kill(int(0.9 * staff));
 }
 
 void TechnicalService::process_year()
@@ -98,6 +107,7 @@ unsigned int TechnicalService::getStaffPriority() {
 
 bool TechnicalService::changeStaff(int delta) {
     staff += delta;
+    TheArk::get_instance()->getPopulation()->getAllClassification()[Technical_Service];
     return true;
 }
 
