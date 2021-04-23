d66eb721dece57b1dd4982fa3d815942b1de0f77
Pain <agibalov.sd@phystech.edu>
2021-04-23 17:57:59 +0300

    Fixed for new array<list<...>>

--- MedicalService.cpp
+++ MedicalService.cpp
@@ -7,7 +7,6 @@
 #include "Population.h"
 #include "BiologicalService.h"
 #include "SocialService.h"
-#include <iostream>
 
 MedicalService::MedicalService() : retirementAge(65), ChildrenDeath(0.0001), AdultDeath(0.001), OldDeath(0.01),
                                    State(100), n_engineers(50), n_scientists(25), resources(1000) {}
@@ -17,32 +16,31 @@ void MedicalService::process_accident(AccidentSeverity as) {
 }
 
 void MedicalService::process_year() {
-    auto p = &TheArk::get_instance()->getPopulation()->people;
+    auto p = &TheArk::get_instance()->getPopulation()->getPeople();
     unsigned int oll_health = 0, IlChild = 0, IlAd = 0, IlOld = 0;
     unsigned int HIlChild = 0, HIlAd = 0, HIlOld = 0;
-    for (auto i = p->begin(); i != p->end(); ++i) {
-        for (auto it = i->begin(); it != i->end(); ++it) {
-            oll_health += it->getPhysicalHealth();
-            if ((it->getPhysicalHealth() < 60) &&
-                (it->getAge() < TheArk::get_instance()->getSocialService()->borderChildrenToAdults())) {
-                if (it->getPhysicalHealth() < 30)
-                    HIlChild++;
-                IlChild++;
-            }
-            if ((it->getPhysicalHealth() < 60) &&
-                (it->getAge() >= TheArk::get_instance()->getSocialService()->borderChildrenToAdults()) &&
-                (it->getAge() <= borderAdultsToOldmen())) {
-                if (it->getPhysicalHealth() < 30)
-                    HIlAd++;
-                IlAd++;
-            }
-            if ((it->getPhysicalHealth() < 60) &&
-                (it->getAge() > borderAdultsToOldmen())) {
-                if (it->getPhysicalHealth() < 30)
-                    HIlOld++;
-                IlOld++;
-            }
+    for (auto it = p->begin(); it != p->end(); ++it) {
+        oll_health += (*it)->getPhysicalHealth();
+        if (((*it)->getPhysicalHealth() < 60) &&
+            ((*it)->getAge() < TheArk::get_instance()->getSocialService()->borderChildrenToAdults())) {
+            if ((*it)->getPhysicalHealth() < 30)
+                HIlChild++;
+            IlChild++;
         }
+        if (((*it)->getPhysicalHealth() < 60) &&
+            ((*it)->getAge() >= TheArk::get_instance()->getSocialService()->borderChildrenToAdults()) &&
+            ((*it)->getAge() <= borderAdultsToOldmen())) {
+            if ((*it)->getPhysicalHealth() < 30)
+                HIlAd++;
+            IlAd++;
+        }
+        if (((*it)->getPhysicalHealth() < 60) &&
+            ((*it)->getAge() > borderAdultsToOldmen())) {
+            if ((*it)->getPhysicalHealth() < 30)
+                HIlOld++;
+            IlOld++;
+        }
+
     }
     State = 1.0 * oll_health / TheArk::get_instance()->getPopulation()->getTotal();
     ChildrenDeath = 1.0 * HIlChild / TheArk::get_instance()->getPopulation()->getChildren();
