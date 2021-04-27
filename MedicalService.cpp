0d18ef20ad0f8df5f45b09960f1368ced83d6c5c
Pain <agibalov.sd@phystech.edu>
2021-04-27 18:01:49 +0300

    Fourth step of realisation process_year. Added comments and Birth rate. Written new functions.

--- MedicalService.cpp
+++ MedicalService.cpp
@@ -8,101 +8,130 @@
 #include "BiologicalService.h"
 #include "SocialService.h"
 #include <cmath>
+#include <iostream>
 
 MedicalService::MedicalService() : retirementAge(65), ChildrenDeath(0.0001), AdultDeath(0.001), OldDeath(0.01),
-                                   State(100), n_engineers(70), resources(1000) {}
+                                   State(100), n_staff(70), resources(1000), Birth(0) {}
 
 void MedicalService::process_accident(AccidentSeverity as) {
 }
 
 void MedicalService::process_year() {
-    unsigned int oll_health = 0, IlChild = 0, IlAd = 0, IlOld = 0;                                          // количество больных
-    unsigned int HIlChild = 0, HIlAd = 0, HIlOld = 0;                                                       // количество тяжелобольных
-    for (auto &it : TheArk::get_instance()->getPopulation()->getPeople()) {                                 // обходим по всем людям на корабле
-        oll_health += it->getPhysicalHealth();                                                              // считаем общий показатель здоровья
+
+    retirementAge = static_cast<unsigned int>(round(51 * (2 - TheArk::get_instance()->getSocialService()->getState() *
+                                                              State * State / 1000000)));
+    n_staff = static_cast<unsigned int>(round(0.035 * TheArk::get_instance()->getPopulation()->getTotal()));
+    resources = 20 * n_staff;
+    Birth = 0.012*TheArk::get_instance()->getPopulation()->getTotal();
+
+    unsigned int oll_health = 0;                                                                                        // общее здоровье всего корабля
+    unsigned int HIlChild = 0, HIlAd = 0, HIlOld = 0;                                                                   // количество тяжелобольных
+    for (auto &it : TheArk::get_instance()->getPopulation()->getPeople()) {                                             // обходим по всем людям на корабле
+        oll_health += it->getPhysicalHealth();                                                                          // считаем общий показатель здоровья
         if (it->getAge() < TheArk::get_instance()->getSocialService()->borderChildrenToAdults()) {
-            if ((it->getPhysicalHealth() < 60) && (it->getPhysicalHealth() > 30)) {
-                IlChild++;                                                                                   // считаем количество больных детей
-                it->setPhysicalHealth(it->getPhysicalHealth() + rand() % 4 - rand() % 3);
+            if ((it->getPhysicalHealth() < 60) && (it->getPhysicalHealth() > 30)) {                                     // обработка детей со здоровьем от 30 до 60
+                it->setPhysicalHealth(static_cast<unsigned int>(round(it->getPhysicalHealth() + rand() % 4 - rand() % 3 +
+                                                        (TheArk::get_instance()->getSocialService()->getState() *
+                                                         State - 90000) / 10000)));
             }
-            if (it->getPhysicalHealth() < 30) {
-                HIlChild++;                                                                                  // считаем количество тяжелобольных детей
-                it->setPhysicalHealth(it->getPhysicalHealth() + rand() % 4 - rand() % 4);
+            if (it->getPhysicalHealth() < 30) {                                                                         // обработка тяжелобольных детей со здоровьем <30
+                HIlChild++;                                                                                             // считаем количество тяжелобольных детей
+                it->setPhysicalHealth(static_cast<unsigned int>(round(it->getPhysicalHealth() + rand() % 4 - rand() % 4 +
+                                            (TheArk::get_instance()->getSocialService()->getState() *
+                                             State - 90000) / 10000)));
             }
-            it->setPhysicalHealth(it->getPhysicalHealth() + rand() % 5 - rand() % 3);
-            if (it->getPhysicalHealth() > 100) it->setPhysicalHealth(100);
+            it->setPhysicalHealth(static_cast<unsigned int>(it->getPhysicalHealth() + rand() % 5 - rand() % 5 +                       // обратоботка детей со здоровьем >60
+                    (TheArk::get_instance()->getSocialService()->getState() *
+                     State - 90000) / 10000));
+            if (it->getPhysicalHealth() > 100) it->setPhysicalHealth(100);                                  // необходимый ограничитель
         }
         if ((it->getAge() >= TheArk::get_instance()->getSocialService()->borderChildrenToAdults()) &&
             (it->getAge() <= borderAdultsToOldmen())) {
             if ((it->getPhysicalHealth() < 60) && (it->getPhysicalHealth() > 30)) {
-                IlAd++;
-                it->setPhysicalHealth(it->getPhysicalHealth() + rand() % 3 - rand() % 3);
+                it->setPhysicalHealth(static_cast<unsigned int>(it->getPhysicalHealth() + rand() % 3 - rand() % 3 +
+                                      (TheArk::get_instance()->getSocialService()->getState() *
+                                       State - 90000) / 10000));
             }
             if (it->getPhysicalHealth() < 30) {
                 HIlAd++;
-                it->setPhysicalHealth(it->getPhysicalHealth() + rand() % 3 - rand() % 4);
+                it->setPhysicalHealth(static_cast<unsigned int>(it->getPhysicalHealth() + rand() % 3 - rand() % 4 +
+                                      (TheArk::get_instance()->getSocialService()->getState() *
+                                       State - 90000) / 10000));
             }
-            it->setPhysicalHealth(it->getPhysicalHealth() + rand() % 4 - rand() % 4);
+            it->setPhysicalHealth(static_cast<unsigned int>(it->getPhysicalHealth() + rand() % 4 - rand() % 4 +
+                                  (TheArk::get_instance()->getSocialService()->getState() *
+                                   State - 90000) / 10000));
             if (it->getPhysicalHealth() > 100) it->setPhysicalHealth(100);
         }
         if (it->getAge() > borderAdultsToOldmen()) {
             if ((it->getPhysicalHealth() < 60) && (it->getPhysicalHealth() > 30)) {
-                IlOld++;
-                it->setPhysicalHealth(it->getPhysicalHealth() + rand() % 2 - rand() % 3);
+                it->setPhysicalHealth(it->getPhysicalHealth() + rand() % 2 - rand() % 4 +
+                                      (TheArk::get_instance()->getSocialService()->getState() *
+                                       State - 90000) / 10000);
             }
             if (it->getPhysicalHealth() < 30) {
                 HIlOld++;
-                it->setPhysicalHealth(it->getPhysicalHealth() + rand() % 2 - rand() % 5);
+                it->setPhysicalHealth(it->getPhysicalHealth() + rand() % 2 - rand() % 5 +
+                                      (TheArk::get_instance()->getSocialService()->getState() *
+                                       State - 90000) / 10000);
             }
-            it->setPhysicalHealth(it->getPhysicalHealth() + rand() % 3 - rand() % 5);
+            it->setPhysicalHealth(it->getPhysicalHealth() + rand() % 3 - rand() % 4 +
+                                  (TheArk::get_instance()->getSocialService()->getState() *
+                                   State - 90000) / 10000);
             if (it->getPhysicalHealth() > 100) it->setPhysicalHealth(100);
         }
     }
-    State = 1.0 * oll_health /
-            TheArk::get_instance()->getPopulation()->getTotal();                                        // вычислятся State исходя из среднего здоровья всего корабля
-    ChildrenDeath = 0.01 * TheArk::get_instance()->getSocialService()->getState() * HIlChild /
-                    TheArk::get_instance()->getPopulation()->getChildren();                             // вычислятся веротность смерти из процента тяжелобольных
-    AdultDeath = 0.05 * TheArk::get_instance()->getSocialService()->getState() * HIlAd /
-                 TheArk::get_instance()->getPopulation()->getAdults();
-    OldDeath = 0.1 * TheArk::get_instance()->getSocialService()->getState() * HIlOld /
-               TheArk::get_instance()->getPopulation()->getOldmen();
 
-    retirementAge = 51 * (2 - TheArk::get_instance()->getSocialService()->getState() *
-                              State);         // вычислятся пенсионный возраст исходя из состояния медицины и социума
+    if (TheArk::get_instance()->getPopulation()->getTotal() != 0) {
+        State = 1.0 * oll_health /
+                TheArk::get_instance()->getPopulation()->getTotal();                                        // вычислятся State исходя из среднего здоровья всего корабля
+    } else State = 0;
+
+    ChildrenDeath = 0.0001 * TheArk::get_instance()->getSocialService()->getState() * HIlChild /
+                    TheArk::get_instance()->getPopulation()->getChildren();                             // вычислятся веротность смерти из процента тяжелобольных
+    AdultDeath = 0.005 * TheArk::get_instance()->getSocialService()->getState() * HIlAd /
+                 TheArk::get_instance()->getPopulation()->getAdults();
+    OldDeath = 0.001 * TheArk::get_instance()->getSocialService()->getState() * HIlOld /
+               TheArk::get_instance()->getPopulation()->getOldmen();
 
 }
 
 void MedicalService::setState(double s) {
-    n_engineers = 0.035 * TheArk::get_instance()->getPopulation()->getTotal();
+    n_staff = 0.035 * TheArk::get_instance()->getPopulation()->getTotal();
     if (s != double(100)) {
         unsigned int delta = (100 - s);
         for (auto &it : TheArk::get_instance()->getPopulation()->getPeople())
             it->setPhysicalHealth((it->getPhysicalHealth() - delta) * (95 + rand() % 10) / 100);
     }
+    State = s;
 }
 
 unsigned int MedicalService::getStaffDemand() {
-    return round((0.035 * TheArk::get_instance()->getPopulation()->getTotal() - n_engineers) * (double(1.8) - State));
+    return round((n_staff - TheArk::get_instance()->getPopulation()->getAllClassification()[2].size()) * (double(1.8) - State));
 }
 
 unsigned int MedicalService::getStaffPriority() {
-    return round(2 * exp(1 - n_engineers));
+    return round(2 * exp(1 - n_staff));
 }
 
-unsigned int MedicalService::borderAdultsToOldmen() {
+unsigned int MedicalService::borderAdultsToOldmen() const {
     return retirementAge;
 }
 
-double MedicalService::deathRateChildren() {
+double MedicalService::deathRateChildren() const {
     return ChildrenDeath;
 }
 
-double MedicalService::deathRateAdult() {
+double MedicalService::deathRateAdult() const {
     return AdultDeath;
 }
 
-double MedicalService::deathRateOldmen() {
+double MedicalService::deathRateOldmen() const {
     return OldDeath;
+}
+
+double MedicalService::BirthRate() const {
+    return Birth;
 }
 
 double MedicalService::getState() {
--- MedicalService.h
+++ MedicalService.h
@@ -12,14 +12,14 @@ using std::list;
 
 class MedicalService : public Service {
 private:
-    unsigned int n_engineers; // количество инженеров (поддержка текущего состояния)
-    unsigned int n_scientists; // количество ученых (повышение эффективности, научные проекты)
-    unsigned int resources; // количество задействованных ресурсов
-    double State;
-    unsigned int retirementAge;
-    double ChildrenDeath;
-    double AdultDeath;
-    double OldDeath;
+    unsigned int n_staff;                                 // количество рабочих всего
+    unsigned int resources;                               // количество задействованных ресурсов
+    double State;                                         // состояние системы по 100-бальной шкале
+    unsigned int retirementAge;                           // возраст старения
+    double ChildrenDeath;                                 // средняя вероятность, что один ребенок подгибнет в течение года
+    double AdultDeath;                                    // средняя вероятность, что один взрослый подгибнет в течение года
+    double OldDeath;                                      // средняя вероятность, что один старик подгибнет в течение года
+    double Birth{};                                         // рождаемость в год
 public:
     MedicalService();
 
@@ -28,17 +28,18 @@ public:
     double getState() override;                           // каждая служба должна уметь вернуть свое состояние в процентах, посчитав его в своих терминах
     void setState(double s) override;                     // функция для инициализации, каждая служба должна уметь получить состояние в процентах и пересчитать  его в своих терминах
 
-    unsigned int borderAdultsToOldmen();    // возраст старения
-    double deathRateChildren();             // средняя вероятность, что один ребенок подгибнет в течение года
-    double deathRateAdult();                // средняя вероятность, что один взрослый подгибнет в течение года
-    double deathRateOldmen();               // средняя вероятность, что один старик подгибнет в течение года
+    unsigned int borderAdultsToOldmen() const;                  // возраст старения
+    double deathRateChildren() const;                           // средняя вероятность, что один ребенок подгибнет в течение года
+    double deathRateAdult() const;                              // средняя вероятность, что один взрослый подгибнет в течение года
+    double deathRateOldmen() const;                             // средняя вероятность, что один старик подгибнет в течение года
+    double BirthRate() const;                                   // рождаемость в год
 
-    unsigned int getResourceDemand();           // сколько ресурсов требуется
-    unsigned int getResourcePriority();         // с каким приоритетом служба будет требовать ресурсы
-    unsigned int getStaffDemand();              // сколько людей требуется
-    unsigned int getStaffPriority();            // с каким приоритетом слуюба будет требовать людей
-    bool changeStaff (int delta);               // сколько людей добавили или забрали (в т.ч. смертность)
-    bool changeResources(int delta);            // сколько ресурсов добавили или забрали (в т.ч. износ)
+    unsigned int getResourceDemand() override;            // сколько ресурсов требуется
+    unsigned int getResourcePriority() override;          // с каким приоритетом служба будет требовать ресурсы
+    unsigned int getStaffDemand() override;               // сколько людей требуется
+    unsigned int getStaffPriority() override;             // с каким приоритетом слуюба будет требовать людей
+    bool changeStaff(int delta) override;                 // сколько людей добавили или забрали (в т.ч. смертность)
+    bool changeResources(int delta) override;             // сколько ресурсов добавили или забрали (в т.ч. износ)
 
 };
 
