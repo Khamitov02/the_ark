c769b54a798a6cecc5ae106219893eb8e793d637
Erik <khamitov02@inbox.ru>
2021-05-02 17:23:37 +0300

    first navi change

--- NavigationService.cpp
+++ NavigationService.cpp
@@ -3,21 +3,125 @@
 //
 
 #include "NavigationService.h"
+//double(TheArk::get_instance()->getPopulation()->getAllClassification()[4].size())
 
-NavigationService::NavigationService()
+NavigationService::NavigationService():State(100),staff(200),
+resources(static_cast<unsigned int>(0.2*(TheArk::get_instance()->getResources()->getComponents()+TheArk::get_instance()->getResources()->getConsumables()))),
+need_staff(200),
+need_resources(static_cast<unsigned int>(0.2*(TheArk::get_instance()->getResources()->getComponents()+TheArk::get_instance()->getResources()->getConsumables()))),
+stage(0),lost_years(0)
 {
 
 }
 
-void NavigationService::process_accident(AccidentSeverity as)
-{
+void NavigationService::process_accident(AccidentSeverity as){int k=0;
+    if(AccidentSeverity::NEGLIGIBLE == as){
+        k = 1;
+
+
+    }
+    if(AccidentSeverity::LIGHT == as){
+        k = 2 ;
+
+
+    }
+    if(AccidentSeverity::MEDIUM == as){
+        k = 3 ;
+
+
+    }
+    if(AccidentSeverity::SEVERE == as){
+        k = 4;
+
+
+    }
+    if(AccidentSeverity::DISASTROUS == as){
+        k = 5;
+
+
+    }
+    if(AccidentSeverity::CATASTROPHIC == as){
+        k = 6;
+
+
+    }
+    this->lost_years += k*(double(TheArk::get_instance()->getYearsTotal()-TheArk::get_instance()->getCurrentYear())/125);
+    this->changeResources(-k*TheArk::get_instance()->getResources()->getComponents()/100);
+    this->changeStaff(-k*2);
 
 }
+void NavigationService::process_year(){
+    this->stage = round((TheArk::get_instance()->getCurrentYear()/TheArk::get_instance()->getYearsTotal())*100);
+    int f=0;
+    /*f=rand() % 100 + 1;
 
+    if (f>33 && this->State<95)                   //шанс на удачное сокращение потерянного на инциденты времени
+    {this->lost_years-=rand()%5+1;
+
+
+    }
+*/
+    if (stage !=0 && (stage<5))
+    {this->changeResources(0);
+    }
+    if (stage >= 5 && stage <=20)
+    {
+        this->changeResources(-TheArk::get_instance()->getResources()->getComponents()/1000);
+    }
+    if (stage >= 20 && stage <=80)
+    {
+        this->changeResources(-TheArk::get_instance()->getResources()->getComponents()/100);
+    }
+    if (stage>100)
+        this->stage=100;
+    if (stage >= 80 && stage <=101)
+    {
+        this->changeResources(0);
+    }
+   // if (this->staff==0) this->State=0;
+    //else
+   // this->State=(100- this->State=(100- (this->lost_years)/TheArk::get_instance()->getYearsTotal()*100);-(this->staff/this->need_staff));
+if (TheArk::get_instance()->getPopulation()->getTotal()==0 || this->State<=0)
+    this->State=0;
+else
+    this->State=100*(1-(this->staff/this->need_staff+lost_years/TheArk::get_instance()->getYearsTotal()));
+
+}
 double NavigationService::getState() {
-    return 100;
+    return this->State;
+
+
 }
 
 void NavigationService::setState(double s) {
+    this->staff=round((s-1)/100*need_staff);
+   
+    this->State = (s-1);
+
 
 }
+
+bool NavigationService:: changeResources(int delta)
+{
+    this->resources += delta;
+    return true;
+}
+
+
+bool NavigationService::changeStaff(int delta)
+{ this->staff += delta;
+    return true;
+}
+
+unsigned int NavigationService::getStaffDemand()
+{
+    //return this->need_staff - this->staff;
+    return (need_staff-staff);
+
+}unsigned int NavigationService::getResourceDemand()
+{
+    //return this->need_resources - this->resources;
+    return 0;
+
+}
+
